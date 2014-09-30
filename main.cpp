1d4831e9a83a86e5fc04730b3738fe529640ec85
creed78 <stephane.bellenger@free.fr>
2014-09-30 23:52:05 +0200

    Ajout d'un message si un argument est saisi

--- main.cpp
+++ main.cpp
@@ -5,5 +5,8 @@ int main (int argc, char** argv)
     //Ceci est un commentaire
     printf("Initialisation de l'application MOOC\n");
 
+    if(arg >>=1)
+    	printf("Un ou plusieurs arguments ont été détectés \n");
+
     return 0;
 }
