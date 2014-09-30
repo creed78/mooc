e58dcca71f5e537f30b1ea48d00561f4b0a9169f
creed78 <stephane.bellenger@free.fr>
2014-09-30 23:53:10 +0200

    Correction du code suite à un problème de compilation

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main (int argc, char** argv)
     //Ceci est un commentaire
     printf("Initialisation de l'application MOOC\n");
 
-    if(arg >>=1)
+    if(arg >=1)
     	printf("Un ou plusieurs arguments ont été détectés \n");
 
     return 0;
